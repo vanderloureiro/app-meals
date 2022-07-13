448b8296eb63ebe980c9906a2ec6e445fa71a7d3
Vanderlei Loureiro <vanderlei.loureiro@evoluum.com.br>
2022-07-13 20:36:46 -0300

    first commit

---
+++ lib/pages/categories_page.dart
@@ -0,0 +1,27 @@
+import 'package:app_meals/components/category_item.dart';
+import 'package:app_meals/data/dummy_data.dart';
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Vamos cozinhar?"),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
+    );
+  }
+}
