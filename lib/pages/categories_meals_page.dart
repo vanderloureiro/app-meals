943c2205e996c0dce142926d7f279e8c0646e9f4
Vanderlei Loureiro <vanderlei.loureiro@evoluum.com.br>
2022-07-17 21:49:51 -0300

    Inicio de card para alimento

--- lib/pages/categories_meals_page.dart
+++ lib/pages/categories_meals_page.dart
@@ -1,3 +1,5 @@
+import 'package:app_meals/components/meal_item.dart';
+import 'package:app_meals/data/dummy_data.dart';
 import 'package:flutter/material.dart';
 
 import '../models/category.dart';
@@ -10,9 +12,18 @@ class CategoriesMealsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)!.settings.arguments as Category;
 
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(title: Text(category.title),),
-      body: Center(child: Text("Receitas por categoria ${category.id}"),),
+      body: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (context, index) {
+            return MealItem(categoryMeals[index]);
+          }
+      ),
     );
   }
 }
