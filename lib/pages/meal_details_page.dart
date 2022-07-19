ba1ac1b8bb5d9160215b3dccef1e59c9b8745861
Vanderlei Loureiro <vanderlei.loureiro@evoluum.com.br>
2022-07-18 23:01:58 -0300

    Imagem e ingredientes da refeicao

--- lib/pages/meal_details_page.dart
+++ lib/pages/meal_details_page.dart
@@ -13,8 +13,49 @@ class MealDetailsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: const Center(
-        child: Text('Detalhes da refeição'),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Ingredientes',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          Container(
+            width: 300,
+            height: 200,
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                      child: Text(
+                          meal.ingredients[index],
+                      ),
+                    ),
+                    color: Theme.of(context).colorScheme.secondary,
+                  );
+                },
+            ),
+          )
+        ],
       ),
     );
   }
