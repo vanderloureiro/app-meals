d99835fbccdf23e8783d6faa7c14a252d89ee30f
Vanderlei Loureiro <vanderlei.loureiro@evoluum.com.br>
2022-07-20 23:11:45 -0300

    Detalhamento de passos de comida

--- lib/pages/meal_details_page.dart
+++ lib/pages/meal_details_page.dart
@@ -13,50 +13,81 @@ class MealDetailsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Image.network(
-                meal.imageUrl,
-                fit: BoxFit.cover
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                  meal.imageUrl,
+                  fit: BoxFit.cover
+              ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 10),
-            child: Text(
-              'Ingredientes',
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-          ),
-          Container(
-            width: 300,
-            height: 200,
-            padding: const EdgeInsets.all(10),
-            margin: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(10)
-            ),
-            child: ListView.builder(
+            _buildTitleSection(context, "Ingredientes"),
+            _buildContainerSection(
+              child: ListView.builder(
                 itemCount: meal.ingredients.length,
                 itemBuilder: (context, index) {
                   return Card(
                     child: Padding(
                       padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
                       child: Text(
-                          meal.ingredients[index],
+                        meal.ingredients[index],
                       ),
                     ),
                     color: Theme.of(context).colorScheme.secondary,
                   );
                 },
+              ),
             ),
-          )
-        ],
+            _buildTitleSection(context, "Passos"),
+            _buildContainerSection(
+              child: ListView.builder(
+                itemCount: meal.steps.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text('${index + 1}'),
+                        ),
+                        title: Text(meal.steps[index]),
+                      ),
+                      const Divider()
+                    ],
+                  );
+                }
+              ),
+            )
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildTitleSection(BuildContext context, String title) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
+    );
+  }
+
+  Widget _buildContainerSection({required Widget child}) {
+    return Container(
+      width: 320,
+      height: 200,
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10)
+      ),
+      child: child,
     );
   }
 }
